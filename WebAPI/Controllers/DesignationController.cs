a548f08cbb97688c8dd65f41ad502069e97311b0
abdulkabeer665 <67089383+abdulkabeer665@users.noreply.github.com>
2020-11-05 00:37:40 +0500

    Add project files.

---
+++ WebAPI/Controllers/DesignationController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using WebAPI.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace WebAPI.Controllers
+{
+    public class DesignationController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable dataTable = null;
+            DesignationModel designModel = new DesignationModel();
+            dataTable = designModel.GetDesignation();
+            return Request.CreateResponse(HttpStatusCode.OK, dataTable);
+        }
+
+        [HttpPost]
+        public string Post(Designation design)
+        {
+            try
+            {
+                string spName = string.Empty;
+                string conString = string.Empty;
+                string message = string.Empty;
+                DataTable dataTable = new DataTable();
+                conString = ConfigurationManager.ConnectionStrings["DoctorDatabase"].ConnectionString;
+                using (SqlConnection sqlconn = new SqlConnection(conString))
+                {
+                    spName = "[dbo].[AddUpdateDesignation]";
+                    using (SqlCommand sqlcmnd = new SqlCommand())
+                    {
+                        sqlcmnd.CommandType = CommandType.StoredProcedure;
+                        sqlcmnd.CommandText = spName;
+                        sqlcmnd.Connection = sqlconn;
+                        sqlcmnd.Parameters.AddWithValue("@DesignationName", design.DesignationName);
+                        //sqlcmnd.Parameters.AddWithValue("@Is_Deleted", design.Is_Deleted);
+                        sqlcmnd.Parameters.Add("@RequestedOutputMessage", SqlDbType.NVarChar, -1);
+                        sqlcmnd.Parameters["@RequestedOutputMessage"].Direction = ParameterDirection.Output;
+                        sqlconn.Open();
+                        sqlcmnd.ExecuteNonQuery();
+                        sqlconn.Close();
+                        message = sqlcmnd.Parameters["@RequestedOutputMessage"].Value.ToString();
+                        using (SqlDataAdapter sqlDataAdap = new SqlDataAdapter(sqlcmnd))
+                        {
+                            sqlDataAdap.Fill(dataTable);
+                        }
+                    }
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "fail";                
+            }
+        }
+        public string Put(Designation design)
+        {
+            try
+            {
+                string spName = string.Empty;
+                string conString = string.Empty;
+                string message = string.Empty;
+                DataTable dataTable = new DataTable();
+                conString = ConfigurationManager.ConnectionStrings["DoctorDatabase"].ConnectionString;
+                using (SqlConnection sqlconn = new SqlConnection(conString))
+                {
+                    spName = "[dbo].[AddUpdateDesignation]";
+                    using (SqlCommand sqlcmnd = new SqlCommand())
+                    {
+                        sqlcmnd.CommandType = CommandType.StoredProcedure;
+                        sqlcmnd.CommandText = spName;
+                        sqlcmnd.Connection = sqlconn;
+                        sqlcmnd.Parameters.AddWithValue("@DesignationId", design.DesignationId);
+                        sqlcmnd.Parameters.AddWithValue("@DesignationName", design.DesignationName);
+                        sqlcmnd.Parameters.AddWithValue("@LastUpdatedOn", design.LastUpdatedOn);
+                        sqlcmnd.Parameters.Add("@RequestedOutputMessage", SqlDbType.NVarChar, -1);
+                        sqlcmnd.Parameters["@RequestedOutputMessage"].Direction = ParameterDirection.Output;
+                        sqlconn.Open();
+                        sqlcmnd.ExecuteNonQuery();
+                        sqlconn.Close();
+                        message = sqlcmnd.Parameters["@RequestedOutputMessage"].Value.ToString();
+                        using (SqlDataAdapter sqlDataAdap = new SqlDataAdapter(sqlcmnd))
+                        {
+                            sqlDataAdap.Fill(dataTable);
+                        }
+                    }
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return "fail";
+
+            }
+        }
+        public string Delete(Designation design)
+        {
+            try
+            {
+                string message = string.Empty;
+                string spName = string.Empty;
+                string conString = string.Empty;
+                DataTable dataTable = new DataTable();
+                conString = ConfigurationManager.ConnectionStrings["DoctorDatabase"].ConnectionString;
+                using (SqlConnection sqlconn = new SqlConnection(conString))
+                {
+                    spName = "[dbo].[AddUpdateDesignation]";
+                    using (SqlCommand sqlcmnd = new SqlCommand())
+                    {
+                        sqlcmnd.CommandType = CommandType.StoredProcedure;
+                        sqlcmnd.CommandText = spName;
+                        sqlcmnd.Connection = sqlconn;
+                        sqlcmnd.Parameters.AddWithValue("@DesignationId", design.DesignationId);
+                        //sqlcmnd.Parameters.AddWithValue("@DesignationName", design.DesignationName);
+                        sqlcmnd.Parameters.AddWithValue("@Is_Deleted", design.Is_Deleted);
+                        sqlcmnd.Parameters.Add("@RequestedOutputMessage", SqlDbType.NVarChar, -1);
+                        sqlcmnd.Parameters["@RequestedOutputMessage"].Direction = ParameterDirection.Output;
+                        sqlconn.Open();
+                        sqlcmnd.ExecuteNonQuery();
+                        sqlconn.Close();
+                        message = sqlcmnd.Parameters["@RequestedOutputMessage"].Value.ToString();
+                        using (SqlDataAdapter sqlDataAdap = new SqlDataAdapter(sqlcmnd))
+                        {
+                            sqlDataAdap.Fill(dataTable);
+                        }
+                    }
+                }
+                    return message;
+            }
+            catch(Exception ex)
+            {
+                return "fail";
+            }
+
+        }
+    }
+}
---
+++ WebAPI/Models/DesignationModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class DesignationModel
+    {
+        public DataTable GetDesignation()
+        {
+            string conString = string.Empty;
+            string spName = string.Empty;
+            DataTable dataTable = new DataTable();
+            conString = ConfigurationManager.ConnectionStrings["DoctorDatabase"].ConnectionString;
+            using (SqlConnection sqlconn = new SqlConnection(conString))
+            {
+                spName = "[dbo].[GetAll_Designations]";
+                using (SqlCommand sqlcmnd = new SqlCommand())
+                {
+                    sqlcmnd.CommandType = CommandType.StoredProcedure;
+                    sqlcmnd.CommandText = spName;
+                    sqlcmnd.Connection = sqlconn;
+                    using (SqlDataAdapter sqlDataAdap = new SqlDataAdapter(sqlcmnd))
+                    {
+                        sqlDataAdap.Fill(dataTable);
+                    }
+                }
+            }
+            return (dataTable);
+        }
+
+        //public int AddUpdateDesignation(Designation design)
+        //{
+        //    string conString = string.Empty;
+        //    string spName = string.Empty;
+        //    DataTable dataTable = new DataTable();
+        //    int myResult = 0;
+        //    conString = ConfigurationManager.ConnectionStrings["DoctorDatabase"].ConnectionString;
+        //    using (SqlConnection sqlconn = new SqlConnection(conString))
+        //    {
+        //        spName = "[dbo].[AddUpdateDesignation]";
+        //        using (SqlCommand sqlcmnd = new SqlCommand())
+        //        {
+        //            sqlcmnd.CommandType = CommandType.StoredProcedure;
+        //            sqlcmnd.CommandText = spName;
+        //            sqlcmnd.Connection = sqlconn;
+        //            sqlcmnd.Parameters.AddWithValue("@DesignationId", design.DesignationId);
+        //            sqlcmnd.Parameters.AddWithValue("@DesignationName", design.DesignationName);
+        //            sqlcmnd.Parameters.AddWithValue("@CreatedOn", design.CreatedOn);
+        //            sqlcmnd.Parameters.AddWithValue("@LastUpdatedOn", design.LastUpdatedOn);
+        //            sqlcmnd.Parameters.AddWithValue("@Is_Deleted", design.Is_Deleted);
+        //            sqlconn.Open();
+        //            myResult = sqlcmnd.ExecuteNonQuery();
+        //            sqlconn.Close();
+        //            using (SqlDataAdapter sqldataAdap = new SqlDataAdapter())
+        //            {
+        //                sqldataAdap.Fill(dataTable);
+        //            }
+        //        }
+        //    }
+        //    return myResult;
+        //}
+    }
+    public class Designation
+    {
+        public Guid DesignationId { get; set; }
+        public string DesignationName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastUpdatedOn { get; set; }
+        public byte Is_Deleted { get; set; }
+    }
+}
